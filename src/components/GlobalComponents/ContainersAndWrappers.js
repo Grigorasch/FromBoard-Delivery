656aa7cd16327ab747154b46a3cd25d58fa1c9d3
Grigorasch <pgv856@gmail.com>
2023-12-27 23:17:57 +0600

    Глобальные компоненты дополнены

---
+++ src/components/GlobalComponents/ContainersAndWrappers.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const SectionContainer = styled.div`
+    margin: 0 auto;
+    padding: 0 20px;
+    width: 1536px;
+    color: #000;
+`
---
+++ src/components/GlobalComponents/GlobalThemeProvider.js
@@ -0,0 +1,3 @@
+export const blackColor = props => props.theme.black;
+export const mainColor = props => props.theme.mainColor;
+export const whiteColor = props => props.theme.white;
